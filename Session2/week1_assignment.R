9cfefdac6ed9934b9857ef05a9ab3ffac0957912
Srinivasa Rao <srinivasarao.rao@gmail.com>
2021-04-21 09:23:15 +0100

    week1 assignment

---
+++ Session2/week1_assignment.R
@@ -0,0 +1,14 @@
+# Q1. Packages can be installed in R from various repositories. The most common ones
+#     are CRAN and Bioconductor. Install the beeswarm package from CRAN.
+
+# Q2. The beeswarm package comes with a preloaded dataset of breast tumours. Load 
+#     this dataset into your global R environment.
+#     hint: You first need to load the package into R and then load the breast data.frame
+#     into memory; try ??breast at your R console and follow the help pages if needed.
+
+# Q3. How many ER positive and ER negative tumours are in the dataset?
+
+# Q4. Plot ESR1 on the X axis and ERBB2 on the y axis, colour the dots by ER status
+
+
+
