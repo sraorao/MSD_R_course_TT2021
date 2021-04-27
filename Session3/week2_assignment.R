6b5754b25f172e59bf24076d5e45611529538948
Srinivasa Rao <srinivasarao.rao@gmail.com>
2021-04-27 11:07:39 +0100

    week2 assignment added

---
+++ Session3/week2_assignment.R
@@ -0,0 +1,17 @@
+# Q1. Install the stemHypoxia data package from Bioconductor by following the 
+#     instructions here: https://bioconductor.org/packages/devel/data/experiment/html/stemHypoxia.html
+
+ 
+
+# Q2. Load the stemHypoxia gene expression data.frame and draw a boxplot of gene expressions for all samples
+#     hint: find the help page for the dataset to get you started. BUT, there is an error 
+#           in the example code, how can you fix it?
+
+ 
+
+# Q3. Make a new column in the gene expression data.frame for the mean expression of each gene across
+#     all the samples. Sort the data.frame from highest to lowest mean expression and subset the top
+#     10 genes (with the highest mean expression).
+#     hint: Useful functions for this task are rowMeans(), base::order()/dplyr::arrange()
+
+
